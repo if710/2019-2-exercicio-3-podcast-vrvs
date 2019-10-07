2f1220ab508c0d2a87f049973bfa3819f363f842
vrvs <vrvs@cin.ufpe.br>
2019-10-07 10:29:24 -0300

    .7 .8 done

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/ItemFeedAdapter.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/ItemFeedAdapter.kt
@@ -1,10 +1,11 @@
 package br.ufpe.cin.android.podcast
 
-import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -33,12 +34,18 @@ class ItemFeedAdapter (private val itemFeeds: List<ItemFeed>, private val main: 
             intent.putExtra("link", episode.link)
             main.startActivity(intent)
         }
+        holder.download.setOnClickListener{
+            Toast.makeText(main.applicationContext, "Downloading ${episode.title}..", Toast.LENGTH_SHORT).show()
+            val downloadService = Intent(main.applicationContext, DownloadService::class.java)
+            downloadService.data = Uri.parse(episode.link)
+            main.startService(downloadService)
+        }
     }
 
     class ViewHolder (item : View) : RecyclerView.ViewHolder(item), View.OnClickListener {
         val title : TextView = item.findViewById(R.id.item_title)
         val date : TextView = item.findViewById(R.id.item_date)
-        private val download : TextView = item.findViewById(R.id.item_action)
+        val download : Button = item.findViewById(R.id.item_action)
 
         init {
             download.setOnClickListener(this)
